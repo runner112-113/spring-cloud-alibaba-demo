6a63ba87ca0e48d2adfbdbbc43f601b28517541c
xhs <xuhuasong@eastmoney.com>
2024-09-26 19:50:10 +0800

    change code architecture

--- cloud-service-application/cloud-service-consumer/src/main/java/com/runner/controller/dubbo/DubboDemoController.java
+++ cloud-service-application/cloud-service-consumer/src/main/java/com/runner/controller/dubbo/DubboDemoController.java
@@ -1,11 +1,22 @@
 package com.runner.controller.dubbo;
 
+import brave.Span;
+import brave.Tracing;
+import brave.propagation.ThreadLocalCurrentTraceContext;
+import brave.propagation.TraceContextOrSamplingFlags;
 import com.runner.interfaces.DemoService;
 import com.runner.interfaces.MyAgentIntercept;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.cloud.sleuth.CurrentTraceContext;
+import org.springframework.cloud.sleuth.TraceContext;
+import org.springframework.cloud.sleuth.brave.bridge.BraveCurrentTraceContext;
+import org.springframework.scheduling.concurrent.ForkJoinPoolFactoryBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.ForkJoinPool;
 
 @RestController
 @MyAgentIntercept
@@ -18,8 +29,33 @@ public class DubboDemoController {
     @DubboReference(version = "1.0.0", group = "dev", timeout = 3000)
     DemoService demoService;
 
+
+    @Resource
+    Tracing tracing;
+
+
+    @Resource
+    BraveCurrentTraceContext braveCurrentTraceContext;
+
     @GetMapping("/dubbo")
     public String helloDubbo() {
+
+        TraceContextOrSamplingFlags parentContext = TraceContextOrSamplingFlags.create(tracing.currentTraceContext().get());
+        Runnable runnableWrapper = braveCurrentTraceContext.wrap(() -> {
+            Span span = tracing.tracer().nextSpan(parentContext).name("consumer-new-span").annotate("consumer start execute").start();
+            try {
+                System.out.println("sleep start...");
+                Thread.sleep(3000);
+                System.out.println("sleep over...");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            span.annotate("consumer end execute").finish();
+        });
+
+        ForkJoinPool.commonPool().execute(runnableWrapper);
+
         System.out.println("hello dubbo");
         return demoService.sayHello("context");
     }
--- cloud-service-application/cloud-service-provider/src/main/java/com/runner/service/dubbo/DemoServiceImpl.java
+++ cloud-service-application/cloud-service-provider/src/main/java/com/runner/service/dubbo/DemoServiceImpl.java
@@ -1,8 +1,15 @@
 package com.runner.service.dubbo;
 
+import brave.Span;
+import brave.Tracing;
 import com.runner.interfaces.DemoService;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.apache.dubbo.config.annotation.Method;
+import org.springframework.cloud.sleuth.brave.bridge.BraveCurrentTraceContext;
+
+import javax.annotation.Resource;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 
 @DubboService(version = "1.0.0", group = "dev", timeout = 3000,
         methods = {
@@ -11,7 +18,39 @@ import org.apache.dubbo.config.annotation.Method;
 public class DemoServiceImpl implements DemoService {
 
 
+    @Resource
+    BraveCurrentTraceContext braveCurrentTraceContext;
+
+    @Resource
+    Tracing tracing;
+
     public String sayHello(String context) {
+
+        Span span = null;
+        try {
+            span = tracing.tracer().nextSpan().name("provider")
+                    .tag("side","provider")
+                    .tag("test-tag","test").start();
+
+            Span finalSpan = span;
+            Runnable runnableWrapper = braveCurrentTraceContext.wrap(() -> {
+            try {
+                finalSpan.annotate("before start execute");
+                System.out.println(Thread.currentThread().getName() + " sleep start...");
+                Thread.sleep(3000);
+                System.out.println(Thread.currentThread().getName() + "sleep over...");
+                finalSpan.annotate("before end execute");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+            ForkJoinPool.commonPool().submit(runnableWrapper).get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }finally {
+            span.finish();
+        }
         return "HELLO " + context;
     }
 
